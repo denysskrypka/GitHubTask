d71afddcfe48c97dad3b41b94fe23a51b729e952
denysskrypka <118266939+denysskrypka@users.noreply.github.com>
2023-04-28 16:08:31 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ПР3_4_завдання_
+namespace ПР3_3_завдання_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            int maxSum = 0;
+            int currentSum = 0;
+            int startIndex = 0;
+            int endIndex = 0;
+            int[] arr = new int[100];
+            Random rand = new Random();
+            for(int i=0; i<arr.Length; i++) 
+            {
+                arr[i] = rand.Next(-150,100);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    currentSum += arr[i];
+                    if (currentSum < maxSum || maxSum == 0)
+                    {
+                        maxSum = currentSum;
+                        endIndex = i;
+                    }
+                }
+                else
+                {
+                    currentSum = 0;
+                }
+            }
 
+            
+            Console.WriteLine("Неперервна послідовність додатних чисел з мінімальною сумою:");
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\nСума елементів: " + maxSum);
+
+            Console.ReadLine();
         }
     }
 }
